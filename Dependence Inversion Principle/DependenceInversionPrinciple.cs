d0fcd22f49ee6f76dffd5f74b30aed9e11534dd0
RD\wufan <adfihvg.fijd@gmail.com>
2017-12-18 11:39:46 +0800

    添加项目文件。

---
+++ Dependence Inversion Principle/DependenceInversionPrinciple.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Yizit.DesignModel.DIP
+{
+    public interface IDriver
+    {
+         void Drive(ICar car);
+        
+    }
+    public interface ICar
+    {
+        void Run();
+        
+    }
+
+    public class Driver : IDriver
+    {
+        private string _name;
+        
+
+        public Driver()
+        {
+
+        }
+        public Driver(string name)
+        {
+            _name = name;
+        }
+
+        public void Drive(ICar car)
+        {
+            
+            Console.Write("{0}驾驶",_name);
+            car.Run();
+        }
+    }
+
+    public class BMWCar : ICar
+    {
+        public void Run()
+        {
+            Console.WriteLine("宝马车开始运行");
+        }
+    }
+
+    public class BENZCar : ICar
+    {
+        public void Run()
+        {
+            Console.WriteLine("奔驰车开始运行");
+        }
+    }
+    public class Client
+    {
+        public static void Main(string[] args)
+        {
+            ICar benz = new BENZCar();
+            IDriver driver = new Driver("tom");
+            driver.Drive(benz);
+            Console.ReadLine();
+        }
+    }
+}
